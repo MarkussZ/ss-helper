985486b4476e16524eba81c9750c1f6549dbb609
MelkyPie <melkypie@gmail.com>
2017-11-08 01:17:53 +0200

    Removed some debug code

--- content.js
+++ content.js
@@ -5,5 +5,3 @@ $("a > b").filter(function() {
 $("a").filter(function() {
     return $(this).text() === "Remonts" || $(this).text() === "Ремонт" || $(this).text() === "куплю" || $(this).text() === "pērku";
 }).parent().parent().remove();
-//console.log($("a:contains('Remonts')").parent().parent().html());
-//$("tr#tr_*").css("background-color", "red");
